cc5920f5fe450d869b0c2933a172620cca7e72fa
root <root@project.docker.com>
2019-02-04 21:59:31 -0500

    First commit

---
+++ app/etc/env.php
@@ -0,0 +1,53 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => '28953754db96f10aabb70e5e009c1780'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2',
+                'username' => 'magento2',
+                'password' => 'q1w2e3r4t5',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'production',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'compiled_config' => 1
+    ],
+    'install' => [
+        'date' => 'Sat, 02 Feb 2019 07:33:35 +0000'
+    ]
+];
